b32d6e43cd2d74e9d4e075da1852de58b6677a91
Shehanshha <145642515+Shehanshha@users.noreply.github.com>
2024-05-07 22:26:54 +0530

    Add files via upload

---
+++ B_3/exchange.scala
@@ -0,0 +1,16 @@
+object exchange{
+def main(args:Array[String]){
+    var num = (-100);
+    if(num==0)
+    {
+        println("number is zero");
+    }
+    else if(num>0)
+    {
+        println("number is positive");
+    }
+    else{
+        println("number is negative");
+    }
+  }
+}
